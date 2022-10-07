46f65c7ac858f299076a30e2d217f0f8f028be7b
hayyaun <hayyanhami@outlook.com>
2022-10-08 02:56:43 +0330

    .

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports.achievements = require('./src/achievements')
+module.exports.constants = require('./src/constants')
+module.exports.enums = require('./src/enums')
+module.exports.functions =  require('./src/functions')
+module.exports.hashes =  require('./src/hashes)'
