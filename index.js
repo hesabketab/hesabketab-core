85224fcf78e4849b20f61636c15266dff3610d06
hayyaun <hayyanhami@outlook.com>
2022-10-14 06:27:20 +0330

    move files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports.achievements = require('./src/achievements')
-module.exports.constants = require('./src/constants')
-module.exports.enums = require('./src/enums')
-module.exports.functions =  require('./src/functions')
-module.exports.hashes =  require('./src/hashes)'
+module.exports.achievements = require('./achievements')
+module.exports.constants = require('./constants')
+module.exports.enums = require('./enums')
+module.exports.functions =  require('./functions')
+module.exports.hashes =  require('./hashes)'
