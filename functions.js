cebf564ea1fc25d3a60d5a9abf6b0a60d9e6367e
hayyaun <hayyanhami@outlook.com>
2022-10-25 20:52:51 +0330

    .

--- functions.js
+++ functions.js
@@ -1,10 +1,1 @@
-const { LANG } = require('./enums');
 
-module.exports.getUserLang = () => {
-  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const mapTZtoLang = { [LANG.FA]: ['Asia/Tehran', 'Asia/Kabul'], [LANG.EN]: [] };
-  for (const lang in Object.keys(mapTZtoLang)) {
-    if (mapTZtoLang[lang].includes(timezone)) return lang;
-  }
-  return LANG.EN;
-};
